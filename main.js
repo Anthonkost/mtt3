8682d8cd87ef4c09035281d884182e0e7c6d0114
Anton Kostyuchenko <antonkostyuchenko@gmail.com>
2025-05-24 23:51:09 +0100

    upd header behavior

--- main.js
+++ main.js
@@ -122,6 +122,22 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   }
   
+
+//header background
+  window.addEventListener('DOMContentLoaded', function () {
+    var header = document.querySelector('.mtt3-header');
+    function checkScroll() {
+      if (window.scrollY < 24) {
+        header.classList.add('bg-hidden');
+      } else {
+        header.classList.remove('bg-hidden');
+      }
+    }
+    checkScroll();
+    window.addEventListener('scroll', checkScroll);
+  });
+
+
 // Sticky BIOS-style CTA button in bottom-right
   const isItRealWrapper = document.createElement('div');
   isItRealWrapper.style.position = 'fixed';
